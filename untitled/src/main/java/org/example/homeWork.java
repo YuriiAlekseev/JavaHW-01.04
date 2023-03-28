f75022be55720dabb7dc2c0b02b1fd457f7f6801
YuriiAlekseev <ash030788@gmail.com>
2023-03-28 23:07:42 +0300

    дополнение

--- untitled/src/main/java/org/example/homeWork.java
+++ untitled/src/main/java/org/example/homeWork.java
@@ -15,6 +15,20 @@ public class homeWork {
         System.out.println(isLeapYear( 1988)); // true
         System.out.println(isLeapYear( 1999)); // false
 
+        // задача 1
+        int [] arrayTask1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println(Arrays.toString(arrayTask1));
+        int [] arr = arrTask1(arrayTask1); 
+        System.out.println(Arrays.toString(arr));
+
+        // задача 2
+        int [] arrayTask2 = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        System.out.println(Arrays.toString(arrayTask2));
+        int [] arr2 = arrTask2(arrayTask2); 
+        System.out.println(Arrays.toString(arr2));
+
+
+
 //        System.out.println(Arrays.stream(createArray(4, 1)));
 //        int[][] array = new int[4][4]; // [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, x], [0, 0, 0, 1]]
 //        int x = array[2][3];
@@ -61,20 +75,21 @@ public class homeWork {
         return false;
     }
 
-    private static int[] createArray(int len, int initialValue) {
-        // должен вернуть массив длины len, каждое значение которого равно initialValue
-//        int[][] array = {{1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0,0,1}}; // [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, x], [0, 0, 0, 1]]
-//        int[] arr = new int [len];
-//        for (int i = 0; i < ; i++) {
-//
-//        }
-        return null;
-    }
+//     private static int[] createArray(int len, int initialValue) {
+//         // должен вернуть массив длины len, каждое значение которого равно initialValue
+// //        int[][] array = {{1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0,0,1}}; // [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, x], [0, 0, 0, 1]]
+// //        int[] arr = new int [len];
+// //        for (int i = 0; i < ; i++) {
+// //
+// //        }
+//         return null;
+//     }
 
 /**
  * 1. Задать целочисленный массив, состоящий из элементов 0 и 1.
  * Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
- *
+
+
  * 2. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
  *
  * 3. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
@@ -82,5 +97,31 @@ public class homeWork {
  * Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
  * * Также заполнить элементы побочной диагонали
  */
+
+//  задача 1
+    private static int[] arrTask1( int [] arrayTask1) {
+        for (int i = 0; i < arrayTask1.length; i++) {
+            if (arrayTask1[i] == 1) {
+                arrayTask1[i] = 0;
+            } 
+            else {
+                arrayTask1[i] = 1;
+            }           
+        }
+        return arrayTask1;
+
+    
+    }
+// задача 2
+private static int[] arrTask2( int [] arrayTask2) {
+    for (int i = 0; i < arrayTask2.length; i++) {
+        if (arrayTask2[i] < 6) {
+            arrayTask2[i] = arrayTask2[i] * 2;
+        }           
+    }
+    return arrayTask2;
+
+
+}
 
 }
